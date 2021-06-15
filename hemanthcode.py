77b312ae55868701c85141a242fbe6407728742a
Hemanth B <hemanth22hemu@gmail.com>
2021-06-15 21:47:45 +0530

    Update hemanthcode.py

--- hemanthcode.py
+++ hemanthcode.py
@@ -2,13 +2,13 @@ import os,sys,glob;
 
 
 def searchfile(user):
-    files = glob.glob('/home/'+user+'/*.py')
+    files = glob.glob('home/'+user+'/*.py')
     for file in files:
         abc_file = os.path.basename(file)
         return abc_file
 
 def fullsearchfile(user):
-    files = glob.glob('/home/'+user+'/*/*.py',recursive=True)
+    files = glob.glob('home/'+user+'/*.py',recursive=True)
     for file in files:
         return file
 
